9f9bc92646a0a83792f9a6da90184a1111332fc8
tucker <tuckerwray84@gmail.com>
2020-12-08 21:32:46 -0500

    feat-- Episodes.js adds a conditional rendering statement as an eroor boundary  - helps tests run smoothly until props load.

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import parse from 'html-react-parser';
+import React from "react";
+import parse from "html-react-parser";
 
 export default function Episodes(props) {
+  if (!props.episodes||props.episodes===null) {
+    return <h2>Fetching data...</h2>;
+  }
   return (
     <div className="episodes">
-      {props.episodes.map(e => (
-        <div className="episode" key={e.id}>
-          {e.image && (
-            <img className="episode-image" src={e.image.medium} alt={e.name} />
-          )}
-          <div className="episode-info">
-            <p className="episode-number">
-              Season {e.season}, Episode {e.number}
-            </p>
-            <h3>{e.name}</h3>
-            {e.summary && parse(e.summary)}
-            <div className="flex-spacer" />
-            <p className="episode-runtime">{e.runtime} minutes</p>
+      {props.episodes &&
+        props.episodes.map((e) => (
+          <div className="episode" key={e.id}>
+            {e.image && (
+              <img
+                className="episode-image"
+                src={e.image.medium}
+                alt={e.name}
+              />
+            )}
+            <div className="episode-info">
+              <p className="episode-number">
+                Season {e.season}, Episode {e.number}
+              </p>
+              <h3>{e.name}</h3>
+              {e.summary && parse(e.summary)}
+              <div className="flex-spacer" />
+              <p className="episode-runtime">{e.runtime} minutes</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
